3985afed682c26d38e6d9d5b977471ae0e0a6e2f
Jason Park <parkjs814@gmail.com>
2018-12-04 20:26:38 -0500

    Support HTTPS

--- app/index.js
+++ app/index.js
@@ -3,15 +3,23 @@ const history = require('connect-history-api-fallback');
 const express = require('express');
 const app = express();
 
-const {
-  apiEndpoint,
-} = require('../environment');
-
 const frontend = require('./frontend');
 const backend = require('./backend');
+
+const {
+  apiEndpoint,
+  credentials,
+} = require('../environment');
+
+if (credentials) {
+  app.use((req, res, next) => {
+    if (req.secure) return next();
+    res.redirect(301, `https://${req.hostname}${req.url}`);
+  });
+}
 app.use((req, res, next) => {
-  if (req.hostname === 'algo-visualizer.jasonpark.me') return res.redirect(301, 'http://algorithm-visualizer.org/');
-  next();
+  if (req.hostname !== 'algo-visualizer.jasonpark.me') return next();
+  res.redirect(301, 'http://algorithm-visualizer.org/');
 });
 app.use(apiEndpoint, backend);
 app.use(history());
